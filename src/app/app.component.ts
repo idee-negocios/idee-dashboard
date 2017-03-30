9ae165f4e4966278f95dfca74b302e3000735acc
Alan <kiriost.s@gmail.com>
2017-03-30 00:38:50 -0300

    Change _theme.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 import { TdMediaService } from '@covalent/core';
 import 'rxjs/add/observable/throw';
+import { MdIconRegistry} from '@angular/material';
 
 import { LoginService } from './shared/services/index';
 
@@ -11,6 +13,13 @@ import { LoginService } from './shared/services/index';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private domSanitizer: DomSanitizer,
+              private iconRegistry: MdIconRegistry) {
+    // Logo
+    this.iconRegistry.addSvgIconInNamespace('assets', 'logo',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/logo1.svg'));
+
+  }
 
 }
