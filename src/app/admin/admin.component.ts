d8d59cf15f8c9a352e3b8bf3f8e4652a67475ccc
Alan <alan.boglioli@gmail.com>
2017-04-04 20:41:51 -0300

    Move td-navigation-drawer from AdminComponent to ToolbarComponent

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -9,19 +9,6 @@ import { Route } from '../models';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-  routes: Route[] = [
-    {icon: 'account_balance', title: 'Contabilidad', route: ['']},
-    {icon: 'wb_cloudy', title: 'Servidores', description: 'Asd', route: ['login']},
-    {icon: 'grid_on', title: 'Calendario', description: 'Asd', route: ['']},
-    {icon: 'account_circle', title: 'Usuarios', description: 'Asd', route: ['']},
-    {icon: 'supervisor_account', title: 'Clientes', description: 'Asd', route: ['']},
-    {icon: 'work', title: 'Proyectos', description: 'Asd', route: ['projects']},
-    {icon: 'extension', title: 'Herramientas', description: 'Asd', route: ['']},
-    {icon: 'folder', title: 'Documentos', description: 'Asd', route: ['']},
-    {icon: 'person_pin', title: 'Empleados', description: 'Asd', route: ['']},
-    {icon: 'trending_up', title: 'Estadísticas', description: 'Asd', route: ['']}
-  ].sort((a, b) => a.title < b.title ? -1 : 1);
-
   constructor(private loginService: LoginService) { }
 
   ngOnInit() {
--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TdMediaService } from '@covalent/core';
 
+import { Route } from '../../models';
 import { LoginService, SidenavService } from '../services';
 
 @Component({
@@ -16,6 +17,19 @@ export class ToolbarComponent implements OnInit, AfterViewInit {
 
   selectedProject: string;
   buttonName: string;
+
+  routes: Route[] = [
+    {icon: 'account_balance', title: 'Contabilidad', route: ['']},
+    {icon: 'wb_cloudy', title: 'Servidores', description: 'Asd', route: ['login']},
+    {icon: 'grid_on', title: 'Calendario', description: 'Asd', route: ['']},
+    {icon: 'account_circle', title: 'Usuarios', description: 'Asd', route: ['']},
+    {icon: 'supervisor_account', title: 'Clientes', description: 'Asd', route: ['']},
+    {icon: 'work', title: 'Proyectos', description: 'Asd', route: ['projects']},
+    {icon: 'extension', title: 'Herramientas', description: 'Asd', route: ['']},
+    {icon: 'folder', title: 'Documentos', description: 'Asd', route: ['']},
+    {icon: 'person_pin', title: 'Empleados', description: 'Asd', route: ['']},
+    {icon: 'trending_up', title: 'Estadísticas', description: 'Asd', route: ['']}
+  ].sort((a, b) => a.title < b.title ? -1 : 1);
 
   constructor(public media: TdMediaService,
               public sidenavService: SidenavService) {}
