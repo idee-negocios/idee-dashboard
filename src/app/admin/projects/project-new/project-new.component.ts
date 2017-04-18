638358737a4c0644ff093befb9a479a56a328bc1
Ricardo Castellan <castellanra@gmail.com>
2017-04-18 20:04:47 -0300

    Section New Project Finished

--- src/app/admin/projects/project-new/project-new.component.ts
+++ src/app/admin/projects/project-new/project-new.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AngularFire, FirebaseObjectObservable} from 'angularfire2';
+import { AngularFire, FirebaseListObservable} from 'angularfire2';
+import { TdDialogService } from '@covalent/core';
+
 @Component({
   selector: 'app-project-new',
   templateUrl: './project-new.component.html',
@@ -9,23 +11,38 @@ import { AngularFire, FirebaseObjectObservable} from 'angularfire2';
 export class ProjectNewComponent implements OnInit {
   newProjectForm: FormGroup;
 
-  item: FirebaseObjectObservable<any>;
+  project: FirebaseListObservable<any>;
 
 
-  constructor(private formBuilder: FormBuilder, public af: AngularFire) {
+  constructor(private formBuilder: FormBuilder, private af: AngularFire, private _dialogService: TdDialogService) {
 
    }
 
   ngOnInit() {
-    this.newProjectForm = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.minLength(4)]],
-      owner: ['', [Validators.required]]
-    });
-    this.item = this.af.database.object('/item');
+    this.buildForm();
+    this.project = this.af.database.list('/project');
   }
 
   save(value:any) {
-    this.item.update({ name: value.name, owner: value.owner });
+    let key= this.project.push({ name: value.name, owner: value.owner }).key;
+      this.openAlert();
+      this.buildForm();
+    }
+  openAlert(): void {
+    this._dialogService.openAlert({
+      message: 'El proyecto ha sido creado con éxito.',
+      disableClose: false, // defaults to false
+      title: 'Listo', //OPTIONAL, hides if not provided
+      closeButton: 'Cerrar', //OPTIONAL, defaults to 'CLOSE'
+    });
   }
-
+  buildForm():void{
+      this.newProjectForm= null;
+      setTimeout(()=>{
+        this.newProjectForm = this.formBuilder.group({
+        name: ['', [Validators.required, Validators.minLength(4)]],
+        owner: ['', [Validators.required]]
+        });
+      },0)
+  }
 }
