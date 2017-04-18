e560f474bdcaad3655e97d19cebcc9ae3be20d28
Alan <alan.boglioli@gmail.com>
2017-04-18 03:32:42 -0300

    Update dependencies (it's using Angular 4 now)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
@@ -21,6 +22,7 @@ import { AdminModule } from './admin/admin.module';
   imports: [
     // Angular modules
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     // Extra modules
