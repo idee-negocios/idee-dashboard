5967d1769b65f7f2ffbe612804459ddd97896e1f
Ricardo Castellan <castellanra@gmail.com>
2017-04-18 01:15:05 -0300

    Add functionality to  new project section

--- src/app/admin/projects/project-new/project-new.component.ts
+++ src/app/admin/projects/project-new/project-new.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { AngularFire, FirebaseObjectObservable} from 'angularfire2';
 @Component({
   selector: 'app-project-new',
   templateUrl: './project-new.component.html',
@@ -9,18 +9,23 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ProjectNewComponent implements OnInit {
   newProjectForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  item: FirebaseObjectObservable<any>;
+
+
+  constructor(private formBuilder: FormBuilder, public af: AngularFire) {
+
+   }
 
   ngOnInit() {
     this.newProjectForm = this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(4)]],
       owner: ['', [Validators.required]]
     });
+    this.item = this.af.database.object('/item');
   }
 
-  createProject() {
-    const values = this.newProjectForm.value;
-    console.log(values);
+  save(value:any) {
+    this.item.update({ name: value.name, owner: value.owner });
   }
 
 }
