043b22c1537cb1f4eb74e65beefdeb98ffcbbbd6
Alan <alan.boglioli@gmail.com>
2017-04-21 17:04:59 -0300

    Fix issue with value not initialized

--- src/app/admin/projects/projects-management/projects-management.component.ts
+++ src/app/admin/projects/projects-management/projects-management.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ITdDataTableColumn } from '@covalent/core';
+
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
@@ -12,18 +16,25 @@ export class ProjectsManagementComponent implements OnInit {
 
   projects: FirebaseListObservable<any>;
   columns: ITdDataTableColumn[] = [
-  { name: 'name',  label: 'Nombre', sortable:true },
+    { name: 'name',  label: 'Nombre', sortable:true },
+    { name: 'owner',  label: 'Propietario', sortable:true },
   ];
-  basicData: any[];
-
+  basicData: any[] = [];
 
   constructor(private af: AngularFire) {}
 
   ngOnInit() {
     this.projects = this.af.database.list('/project');
-    this.projects.forEach(project => {
-    this.basicData.push(project);
-});
+
+    this.projects.subscribe(projects => {
+      // projects is an array
+      this.basicData = projects.map(project => {
+        return {
+          name: project.name,
+          owner: project.owner
+        };
+      });
+    });
   }
 
 }
